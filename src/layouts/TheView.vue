aba60b6e87700149a75c8d6376bd6eeb2d355c4f
Cho Hyodong <toletrue@gmail.com>
2023-05-22 20:14:07 +0900

    5.22 작업 in 집

---
+++ src/layouts/TheView.vue
@@ -0,0 +1,9 @@
+<template>
+	<main class="container py-4">
+		<RouterView></RouterView>
+	</main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<h2>Home View</h2>
+		<p>{{ $route.path }}</p>
+		<p>{{ $route.name }}</p>
+		<button class="btn btn-primary" @click="goAboutPage">About으로 이동</button>
+	</div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goAboutPage = () => {
+	router.push({
+		name: 'About',
+	});
+};
+</script>
+
+<style lang="scss" scoped></style>
