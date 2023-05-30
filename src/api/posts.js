1f002138b7fa55b96ec845f8b0ca95e9e447cc52
Cho Hyodong <toletrue@gmail.com>
2023-05-30 19:10:19 +0900

    5.30 작업: 5-2 TransitionGroup 컴포넌트 까지

--- src/api/posts.js
+++ src/api/posts.js
@@ -14,8 +14,12 @@ export function createPost(data) {
 	return posts.post('/', data);
 }
 
+// export function updatePost(id, data) {
+// 	return posts.put(`/${id}`, data);
+// }
+
 export function updatePost(id, data) {
-	return posts.put(`/${id}`, data);
+	return posts.patch(`/${id}`, data);
 }
 
 export function deletePost(id) {
--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -4,7 +4,7 @@
 		<hr class="my-4" />
 		<PostForm
 			v-model:title="form.title"
-			v-model:content="form.title"
+			v-model:content="form.content"
 			@submit.prevent="edit"
 		>
 			<template #actions>
@@ -18,6 +18,7 @@
 				<button class="btn btn-primary">수정</button>
 			</template>
 		</PostForm>
+		<AppAlert :items="alerts" />
 	</div>
 </template>
 
@@ -26,6 +27,7 @@ import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { getPostById, updatePost } from '@/api/posts';
 import PostForm from '@/components/posts/PostForm.vue';
+import AppAlert from '@/components/AppAlert.vue';
 
 const route = useRoute();
 const router = useRouter();
@@ -43,6 +45,7 @@ const fetchPost = async () => {
 		setForm(data);
 	} catch (error) {
 		console.error(error);
+		vAlert(error.message);
 	}
 };
 const setForm = ({ title, content }) => {
@@ -54,13 +57,22 @@ fetchPost();
 const edit = async () => {
 	try {
 		await updatePost(id, { ...form.value });
-		router.push({ name: 'PostDetail', params: { id } });
+		vAlert('수정이 완료되었습니다!!!', 'success');
 	} catch (error) {
 		console.error(error);
+		vAlert(error.message);
 	}
 };
 
 const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } });
+
+const alerts = ref([]);
+const vAlert = (message, type = 'error') => {
+	alerts.value.push({ message, type });
+	setTimeout(() => {
+		alerts.value.shift();
+	}, 2000);
+};
 </script>
 
 <style lang="scss" scoped></style>
